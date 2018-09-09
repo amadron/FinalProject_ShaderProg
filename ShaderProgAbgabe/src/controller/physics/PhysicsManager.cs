669dda0b3759c2af0bb755581b27ec98b8519d59
Benedict Roth <benedict-roth@live.de>
2018-09-10 00:27:58 +0200

    Started to work on Physics

---
+++ ShaderProgAbgabe/src/controller/physics/PhysicsManager.cs
@@ -0,0 +1,59 @@
+﻿using Example.src.model.physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.src.controller.physics
+{
+    class PhysicsManager
+    {
+        public static bool SphereIntercts(SphereCollider sphere, SphereCollider other)
+        {
+            double deltaX = sphere.position.X - other.position.X;
+            deltaX *= deltaX;
+            double deltaY = sphere.position.Y - other.position.Y;
+            deltaY *= deltaY;
+            double deltaZ = sphere.position.Z - other.position.Z;
+            deltaZ *= deltaZ;
+            double sum = deltaX + deltaZ + deltaY;
+
+            double radi = sphere.radius + other.radius;
+            radi *= radi;
+            
+            return sum <= radi;
+        }
+
+        public static bool BoxIntersects(BoxCollider box, BoxCollider other)
+        {
+            Vector3 minA = box.GetMinBounds();
+            Vector3 minB = other.GetMinBounds();
+            Vector3 maxA = box.GetMaxBounds();
+            Vector3 maxB = other.GetMaxBounds();
+            if(!TestBoxAxis(minA.X, minB.X, maxA.X, maxB.X))
+            {
+                return false;
+            }
+            else if(!TestBoxAxis(minA.Y, minB.Y, maxA.Y, maxB.Y))
+            {
+                return false;
+            }
+            else if(!TestBoxAxis(minA.Z, minB.Z, maxA.Z, maxB.Z))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+
+        private static bool TestBoxAxis(float minA, float minB, float maxA, float maxB)
+        {
+            return (minA < maxB && maxA > minB);
+        }
+    }
+}
---
+++ ShaderProgAbgabe/src/model/physics/BoxCollider.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Zenseless.Geometry;
+
+namespace Example.src.model.physics
+{
+    class BoxCollider
+    {
+        public BoxCollider()
+        {
+            this.position = Vector3.Zero;
+        }
+        public BoxCollider(Vector3 position)
+        {
+            this.position = position;
+        }
+
+        public BoxCollider(Vector3 position, float sizeX, float sizeY, float sizeZ)
+        {
+            this.position = position;
+            SetSizeX(sizeX);
+            SetSizeY(sizeY);
+            SetSizeZ(sizeZ);
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            this.position = position;
+            SetSizes(sizeX, sizeY, sizeZ);
+        }
+
+        public void SetSizes(float sizeX, float sizeY, float sizeZ)
+        {
+            SetSizeX(sizeX);
+            SetSizeY(sizeY);
+            SetSizeZ(sizeZ);
+        }
+
+        public void SetSizeX(float sizeX)
+        {
+            this.sizeX = sizeX;
+            float half = sizeX / 2;
+            this.minBounds.X = position.X - half;
+            this.maxBounds.X = position.X + half;
+        }
+
+        public void SetSizeY(float sizeY)
+        {
+            this.sizeY = sizeY;
+            float half = sizeY / 2;
+            this.minBounds.Y = position.Y - half;
+            this.maxBounds.Y = position.Y + half;
+        }
+
+        public void SetSizeZ(float sizeZ)
+        {
+            this.sizeZ = sizeZ;
+            float half = sizeZ / 2;
+            this.minBounds.Z = position.Z - half;
+            this.maxBounds.Z = position.Z + half;
+        }
+
+        public Vector3 GetMinBounds()
+        {
+            return minBounds;
+        }
+
+        public Vector3 GetMaxBounds()
+        {
+            return maxBounds;
+        }
+
+        public float sizeX;
+        public float sizeY;
+        public float sizeZ;
+
+        public Vector3 minBounds;
+        public Vector3 maxBounds;
+
+        public Vector3 position;
+    }
+}
