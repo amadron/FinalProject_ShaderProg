7f684330c36efeb25e288a57a20b91d7839a96d8
Benedict Roth <benedict-roth@live.de>
2018-09-17 22:32:32 +0200

    Working on UI

---
+++ ShaderProgAbgabe/src/controller/rendering/UIRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Example.src.model.graphics.ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenseless.HLGL;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Example.src.controller.rendering
+{
+    class UIRenderer
+    {
+        IShaderProgram sceneShader;
+        IShaderProgram uiShader;
+
+        List<UIElement> elements;
+
+        public void Render(ITexture2D sceneTexture)
+        {
+
+        }
+
+        void RenderScene(ITexture2D scene)
+        {
+            sceneShader.Activate();
+            scene.Activate();
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+            scene.Deactivate();
+            sceneShader.Deactivate();
+        }
+
+        void RenderUIElements()
+        {
+            uiShader.Activate();
+
+            uiShader.Deactivate();
+        }
+        
+    }
+}
---
+++ ShaderProgAbgabe/src/model/entitys/Terrain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Zenseless.HLGL;
+
+namespace Example.src.model.entitys
+{
+    class Terrain : Entity
+    {
+        public Terrain(Bitmap texture)
+        {
+            this.texture = texture;
+        }
+
+        Bitmap texture;
+        float pixelWorldWidth;
+        float pixelWorldHeight;
+        float sizeX;
+        float sizeY;
+        float scale;
+
+        void CalculatePixelSizes()
+        {
+            int resX = texture.Width;
+            int resY = texture.Height;
+            pixelWorldWidth = sizeX / resX;
+            pixelWorldHeight = sizeY / resY;
+        }
+
+        float GetHeightInTerrain(Vector3 pos)
+        {
+            int x = (int)(pos.X / pixelWorldWidth);
+            int y = (int)(pos.Z / pixelWorldHeight);
+            float textVal = texture.GetPixel(x, y).R;
+            return textVal * scale;
+        }
+
+    }
+}
